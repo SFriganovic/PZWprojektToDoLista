624b68bce3478e51e3537592888711f153420411
SFriganovic <121201555+SFriganovic@users.noreply.github.com>
2023-02-08 20:37:26 +0100

    Add files via upload

--- todo_list/base/tests.py
+++ todo_list/base/tests.py
@@ -1,27 +1,3 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from base.views import CustomLoginView, RegisterPage, TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+from django.test import TestCase
 
-
-class TestUrls(SimpleTestCase):
-
-    def test_homepage_url_is_resolved(self):
-        url = reverse('homepage')
-        # print(resolve(url))
-
-        self.assertEquals(resolve(url).func, homepage)
-
-    def test_books_url_is_resolved(self):
-        url = reverse('books')
-
-        self.assertEquals(resolve(url).func.view_class, BookList)
-
-    def test_authors_url_is_resolved(self):
-        url = reverse('authors')
-
-        self.assertEquals(resolve(url).func.view_class, AuthorList)
-
-    def test_authors_url_is_resolved(self):
-        url = reverse('author_q', args=['some-author'])
-
-        self.assertEquals(resolve(url).func.view_class, AuthorBookList)
+# Create your tests here.
